53f9487c33483fc92499ffe14c15611dd9147c05
j541 <52905848+j541@users.noreply.github.com>
2019-07-15 11:49:44 +0530

    Create vowelorconsonent.c

---
+++ vowelorconsonent.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char n,a,e,i,o,u;
+scanf("%c",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+printf("The letter is a vowel");
+else
+printf("The letter is a consonent");
+}
